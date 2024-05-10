892a921f3696ffe44f6c48bbe577b0bcc5a7c679
KPPratiksha <136451304+KPPratiksha@users.noreply.github.com>
2024-05-10 13:18:13 +0530

    Added files for model

---
+++ Contact.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsAPI.Models
+{
+    /// <summary>
+    /// Contact class for storing the contact of the user 
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Unique identifier for contact 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the contact person
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Email id detail of the contact
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Phone number of the contact
+        /// </summary>
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+    }
+}
